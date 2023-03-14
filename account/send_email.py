30cfe6ea331704ec4e7793ff1669102289e8230e
Kydyrbek97 <Kydyrali_097@mail.ru>
2023-03-15 00:19:45 +0600

    added account,celery/redis,swagger,cors

---
+++ account/send_email.py
@@ -0,0 +1,32 @@
+from django.core.mail import send_mail
+
+
+def send_confirmation_email(user, code):
+    full_link = f'http://localhost:8000/api/v1/account/activate/{code}/'
+    # full_link_server = f'http://34.81.142.249/api/v1/accounts/activate/{code}/'
+    send_mail(
+        'Здравствуйте активируйте ваш аккаунт!',
+        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link}',
+        'kydyrbek.mamatkulov@gmail.com',
+        [user],
+        fail_silently=False
+    )
+
+
+def send_reset_email(user):
+    code = user.activation_code
+    email = user.email
+    send_mail('Letter with password rest code!',
+              f'Your reset code {code}', 'from@example.com',
+              [email, ], fail_silently=False)
+
+
+def send_notification(user_email, order_id, price):
+    send_mail('Уведомления о создание заказа!',
+              f''' Вы создали заказ №{order_id}, ожидайте звонка!
+               Полная стоимость вашего заказа: {price}.
+                Спасибо за то что выбрали нас!''',
+              "from@example.com",
+              [user_email, ],
+              fail_silently=False
+              )
---
+++ account/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from account import views
+from account.views import LogoutView
+
+urlpatterns = [
+    path('', views.UserListApiView.as_view()),
+    path('register/', views.RegistrationView.as_view()),
+    path('activate/<uuid:activation_code>/', views.ActivationView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('forgot/', views.ForgotPasswordView.as_view()),
+    path('restore/', views.RestorePasswordView.as_view()),
+]
